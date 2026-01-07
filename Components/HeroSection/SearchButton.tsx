c390af816a86432623ccff96c598010283d3000f
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-01-07 14:47:21 +0800

    update color theme

--- Components/HeroSection/SearchButton.tsx
+++ Components/HeroSection/SearchButton.tsx
@@ -9,7 +9,7 @@ const SearchButton = ({onSearch}:SearchButtonProps) => {
   return (
     <button
       onClick={onSearch}
-      className="w-full h-12 sm:h-14 flex items-center justify-center gap-2 bg-brand-primary border border-brand-primary text-white rounded-full transition-all duration-200 focus:outline-none"
+      className="w-full h-12 sm:h-14 flex items-center justify-center gap-2 bg-brand-primary hover:bg-brand-primaryDark border border-brand-primary text-white rounded-full focus:outline-none"
       aria-label="Search"
     >
       <Search className="w-5 h-5 sm:w-6 sm:h-6 text-white" />
