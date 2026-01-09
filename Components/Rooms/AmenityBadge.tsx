698e071f663049ce9b80cd26bd09d147ef31247f
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-01-09 16:07:34 +0800

    add badge icon

--- Components/Rooms/AmenityBadge.tsx
+++ Components/Rooms/AmenityBadge.tsx
@@ -19,8 +19,9 @@ const AmenityBadge = ({amenity}:AmenityBadgeProps) => {
     }
 
   return (
-    <div className="flex items-center gap-2 px-3 py-1 bg-blue-50 text-blue-700 rounded-full text-xs sm:text-sm">{getIcon(amenity)}
-    <span>{amenity}</span>
+    <div className="flex items-center gap-2 px-3 py-1 bg-blue-50 dark:bg-blue-900/30 text-blue-700 dark:text-blue-300 rounded-full text-xs sm:text-sm">
+      {getIcon(amenity)}
+      <span>{amenity}</span>
     </div>
   )
 }
