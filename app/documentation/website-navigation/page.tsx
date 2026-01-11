5bb465ef71e7e1158780e8de9331444eb560da99
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-01-11 16:43:55 +0800

    Create page to import the content of website navigation

---
+++ app/documentation/website-navigation/page.tsx
@@ -0,0 +1,10 @@
+import { GitWorkflowLayout } from '@/Components/Documentation';
+import WebsiteNavigationContent from '@/Components/Documentation/WebsiteNavigationContent';
+
+export default function WebsiteNavigationPage() {
+  return (
+    <GitWorkflowLayout>
+      <WebsiteNavigationContent />
+    </GitWorkflowLayout>
+  );
+}
