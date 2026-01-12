37b13efc2ed6227992ca69afef512ef67a6df366
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-01-12 14:24:37 +0800

    Fix the error for deployment

--- app/api/admin/activity-stats/route.ts
+++ app/api/admin/activity-stats/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { getActivityStats } from "@/backend/controller/activityLogController";
 import { createEdgeRouter } from "next-connect";
 
-const router = createEdgeRouter<NextRequest, any>()
+const router = createEdgeRouter<NextRequest, { params: Record<string, string> }>()
 router.get(getActivityStats);
 
-export async function GET(request: NextRequest, ctx: any): Promise<NextResponse> {
-  return router.run(request, ctx) as Promise<NextResponse>;
+export async function GET(request: NextRequest, { params }: { params: Record<string, string> }): Promise<NextResponse> {
+  return router.run(request, { params }) as Promise<NextResponse>;
 }
