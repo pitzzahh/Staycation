4acc8d6138fc3caf6970578287b605dac10521bf
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-04 13:36:05 +0800

    Fix layout to display icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,10 +94,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16v2.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32v2.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/apple-touch-iconv2.png', sizes: '180x180', type: 'image/png' },
     ],
   },
 };
@@ -115,9 +116,10 @@ export default function RootLayout({
           async
           defer
         />
+        <link rel="icon" href="/favicon-16x16v2.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32v2.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16 32x32" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-iconv2.png" />
         <link rel="manifest" href="/site.webmanifest" />
         
         {/* Structured Data for SEO */}
